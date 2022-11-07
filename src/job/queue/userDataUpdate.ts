5a567fdcc135261f9dda14b499fc587162a61a69
Siddhanta Gupta <siddhanta.gupta@squareboat.com>
2022-11-07 17:46:12 +0530

    improved keeping updated data in sync

--- src/job/queue/userDataUpdate.ts
+++ src/job/queue/userDataUpdate.ts
@@ -2,6 +2,7 @@ import { Job } from '@libs/sq-nest-queue';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JobModuleConstants } from '../constants';
+import { IJobModel } from '../interfaces';
 import { CandidateJobsService } from '../providers';
 import {
   ApplicationRepositoryContract,
@@ -18,15 +19,28 @@ export class UserUpdateService {
   async updateUserData(data: Record<string, any>) {
     const user = data;
     const userRoles = this.config.get('settings.roles');
+    console.log('user updated', data, user.role === userRoles.recruiter);
     if (user.role === userRoles.recruiter) {
-      await this.candidateJobService.repo.updateAndReturn(
+      let updatedJobs = (await this.candidateJobService.repo.updateAndReturn(
         {
           postedBy: user.id,
         },
         {
           isActive: user.isActive,
         },
-      );
+      )) as IJobModel[];
+      for (let jobIndex = 0; jobIndex < updatedJobs.length; jobIndex++) {
+        let updatedApplication =
+          await this.candidateJobService.applicationRepo.updateAndReturn(
+            {
+              jobId: updatedJobs[jobIndex].id,
+            },
+            {
+              isActive: updatedJobs[jobIndex].isActive,
+            },
+          );
+        console.log(updatedApplication);
+      }
     } else if (user.role === userRoles.candidate) {
       await this.candidateJobService.applicationRepo.updateAndReturn(
         {
