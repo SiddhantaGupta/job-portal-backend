d09abe6f11b57f477162d57572b4656a8f0fd6c3
Siddhanta Gupta <siddhanta.gupta@squareboat.com>
2022-11-14 16:35:41 +0530

    integrated sentry

--- libs/boat/src/rest/restServer.ts
+++ libs/boat/src/rest/restServer.ts
@@ -4,6 +4,10 @@ import { ServerOptions } from './interfaces';
 import { ConfigService } from '@nestjs/config';
 import { RequestGuard } from './guards';
 import { ExceptionFilter } from '../exceptions';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+import { TimeoutInterceptor } from './timeoutInterceptor';
+import { SentryInterceptor } from './sentryInterceptor';
 
 export class RestServer {
   private module: any;
@@ -27,6 +31,29 @@ export class RestServer {
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new ExceptionFilter(httpAdapter));
     options.globalPrefix && app.setGlobalPrefix(options.globalPrefix);
+
+    const server = app.getHttpServer();
+
+    Sentry.init({
+      dsn: process.env.APP_SENTRY_DSN,
+      environment: process.env.APP_ENV,
+      enabled: process.env.APP_ENV === 'local',
+      tracesSampleRate: Number(process.env.APP_SENTRY_SAMPLE_RATE),
+      integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({
+          app: server._events.request_router,
+        }),
+        new Tracing.Integrations.Mysql(),
+      ],
+    });
+
+    app.use(Sentry.Handlers.requestHandler());
+    app.use(Sentry.Handlers.tracingHandler());
+    app.useGlobalInterceptors(
+      new TimeoutInterceptor(),
+      new SentryInterceptor(),
+    );
 
     const config = app.get(ConfigService, { strict: false });
     await app.listen(options.port || config.get<number>('app.port'));
